b637e2772e5d08e856d47a3dc497e1cde50e6102
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-01-25 15:01:59 +0600

    `🧱 build(tsdown):` reconfigured tsdown to bundle external package(s)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
 	exports: true,
 	unbundle: false,
-	external: [],
+	noExternal: [
+		'nhb-toolbox',
+		'nhb-toolbox/hashhash',
+		'nhb-toolbox/utils/types',
+		'nhb-toolbox/hash/types',
+		'nhb-toolbox/object/types',
+	],
 	treeshake: true,
 });
