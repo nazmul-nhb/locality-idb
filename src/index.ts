b5e9e0fd94b18006238b9aebcb2a9462bfe9f636
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-01-25 15:11:48 +0600

    `🔧 update(exports):` re-exported all the required utils

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export { Locality } from './client';
 export type * from './types';
+
+export { Locality } from './client';
+export { Column, Table } from './core';
+export { col, defineSchema, table } from './schema';
+export { openDBWithStores } from './utils';
