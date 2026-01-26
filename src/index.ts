bf7f80153fbb109258d18f5a7ba296a60479da43
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-01-27 02:31:56 +0600

    `🔧 update:` removed `Column` and `Table` classes from export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type * from './types';
 
 export { Locality } from './client';
-export { Column, Table } from './core';
+// export { Column, Table } from './core';
 export { column, defineSchema, table } from './schema';
 export { openDBWithStores } from './utils';
